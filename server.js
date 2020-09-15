e7b5b82c7887627ec4abbd832479bf44f5c48ec1
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-09-15 11:04:32 -0500

    modified server.js code format

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ var app = express();
 // Set initial port to be used in the listener
 var PORT = process.env.PORT || 4000;
 
-// Express.static built-in middleware function to serve static files
-app.use(express.static('public'));
-
 
 // Set up Express app to handle data parsing
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+// Express.static built-in middleware function to serve static files
+app.use(express.static('public'));
 
 
 // Points server to routes for how to respond when users wisit or request data
