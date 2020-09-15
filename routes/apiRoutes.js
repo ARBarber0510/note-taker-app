935705527059ae2a33e7933fe15a49b673c1618f
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-09-15 10:06:07 -0500

    created api GET, POST, DELETE functions in apiRoutes file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,25 @@
 // Linking routes to notes data source.
 var notesData = require("../notesData.js");
+
+
+// Routing
+
+module.exports = function(app) {
+
+    app.get("/api/notes", function(req, res) {
+        res.json(notesData);
+    });
+
+
+    app.post("/api/notes", function(req, res) {
+        notesData.push(req.body);
+        res.json(true);
+    });
+
+
+    app.post("/api/delete", function(req, res) {
+        notesData.length = 0;
+
+        res.json({ ok: true});
+    });
+}
