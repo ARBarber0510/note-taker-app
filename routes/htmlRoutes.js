5a0d67488ee140d0f322ac070da60416ace2ec13
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-09-15 10:12:01 -0500

    added dependcies and HTML get requests to htmlRoutes file

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+// Dependancies
+var path = require("path");
+
+// Routing
+
+module.exports = function(app) {
+
+    app.get("/notes", function(req, res) {
+        res.sendFile(path.join(__dirname, "../notes.html"));
+    });
+
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../index.html"));
+    });
+}
